6d76b49a923d2e1ec7879fc31acb7e86d3c8ce24
Benjamin Ortiz <hello.ben.ortiz@gmail.com>
2018-06-04 20:06:04 -0700

    Build to GH Pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
   plugins: [
-    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
